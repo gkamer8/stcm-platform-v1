830cd2a24bb6804b4a882a627d4971bb958179ea
Gordon Kamer <gkamer@outlook.com>
2021-04-02 23:17:10 -0400

    speedy

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'Bruh bruh bruh'
+
+if __name__ == '__main__':
+    app.run();
---
+++ speedy.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "TODO" << endl;
+
+    return 0;
+}
