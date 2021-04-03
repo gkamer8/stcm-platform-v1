5ef7901ff0ef98a25cd9befe1fa924a0b7b2f4cf
Gordon Kamer <gkamer@outlook.com>
2021-04-03 13:13:14 -0400

    templates

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return 'Bruh bruh bruh'
+
+@app.route('/lookup')
+def lookup():
+    return render_template('lookup.html')
 
 if __name__ == '__main__':
     app.run()
