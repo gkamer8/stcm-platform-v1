fe0d166dd6355cf025eeefa5c0082070802bc533
Gordon Kamer <gkamer@outlook.com>
2021-04-03 00:49:49 -0400

    speedy explanation

--- speedy.cpp
+++ speedy.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/*
+
+This is for anything we want to do speedily, like creating stats from all the stocks and ranking them.
+I wanted to include this mostly for fun so I could try out pipelining b/w Python and C++.
+
+I also might make a GUI application for myself in C++.
+
+*/
+
 using namespace std;
 
 int main(){
