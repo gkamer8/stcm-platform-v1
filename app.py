1d0182c3524d679605bcc00b311ec5bba9125db1
Gordon Kamer <gkamer@outlook.com>
2021-04-03 13:39:24 -0400

    search bar

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ app = Flask(__name__)
 def root():
     return 'Bruh bruh bruh'
 
-@app.route('/lookup')
+@app.route('/lookup', methods=('GET', 'POST'))
 def lookup():
+
+    # On search: 
+    # If it's a ticker, show the stock
+    # If it's not a ticker, search for the company with the API and present a list of options
+
     return render_template('lookup.html')
 
 if __name__ == '__main__':
